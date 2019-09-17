16d64f6cefd0e6c780c613f34f535c78ed1595d3
Leonardo Amado <leonardo@ai-mac-pro.local>
2019-09-17 11:34:12 -0300

    Changed threshold to 0.001, map to medium and added the atributte for R+ in the description

--- common.py
+++ common.py
@@ -11,7 +11,7 @@ DEBUG       = lambda: logging.getLogger().level == logging.DEBUG
 IS_DEBUG    = DEBUG()
 
 MAX_TRAINING_EPISODES = 10000
-CONVERGENCE_THRESHOLD = 0.05
+CONVERGENCE_THRESHOLD = 0.001
 
 # Frames per second (more means faster)
 FPS         = 60
